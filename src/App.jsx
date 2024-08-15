7db15d3749feb83499f21ec17daa1e101826faf2
mrmrs <mr.adam.morse@gmail.com>
2024-08-14 22:00:34 -0700

    Update to fix some bitrot

---
+++ src/App.jsx
@@ -0,0 +1,105 @@
+import { useState } from 'react'
+import MusicPlayer from './components/MusicPlayer'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+  const mrsjxnTracks = [
+    { title: 'No Love', url: '/mrsjxn-no-love.mp3', time: '5:00'},
+    { title: 'Valis', url: '/mrsjxn-valis.mp3', time: '4:30'},
+    { title: 'Minibaar', url: '/mrsjxn-minibaar.mp3', time: '5:36'},
+    { title: 'Touch This', url: '/mrsjxn-touch-this.mp3', time: '6:35'},
+    { title: 'homeworkrevolution808', url: '/mrsjxn-homeworkrevolution808.mp3', time: '3:11'},
+    { title: 'BBB', url: '/mrsjxn-BBB.mp3', time: '3:00'},
+    { title: 'SRCD Party', url: '/mrsjxn-SRCD-Party.mp3', time: '3:50'},
+    { title: 'Morning Bounce', url: '/mrsjxn-morning-bounce.mp3', time: '2:30'},
+    { title: 'Space Owl', url: '/mrsjxn-space-owl.mp3', time: '2:40'},
+  ]
+
+  const jxnTracks = [
+    { title: 'Let Go', url: '/jxnblk-MB-166.mp3', time: '5:00'},
+    { title: 'Really Wanna', url: '/jxnblk-MB-165.mp3', time: '4:30'},
+    { title: 'MMMXIII', url: '/jxnblk-mmmxiii.mp3', time: '20:13'},
+  ]
+
+  const mrsTracks = [
+    { title: 'Do it Like', url: '/mrmrs-do-it-like.mp3', time: '4:26'},
+    { title: 'You & I Go Back', url: '/mrmrs-you-and-i-go-back.mp3', time: '2:31'},
+    { title: 'Freak', url: '/mrmrs-freak.mp3', time: '2:29'},
+    { title: 'The Sunshine Hits', url: '/mrmrs-sunshine-hits.mp3', time: '3:10'},
+  ]
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
+  const [tracks, setTracks] = useState(mrsjxnTracks)
+  const [mrmrsViewBox, setMrmrsViewBox] = useState(64)
+  const [jxnViewBox, setJxnViewBox] = useState(64)
+
+  const handleMrs = () => {
+    setTracks(mrsTracks)
+    setMrmrsViewBox(128)
+    setJxnViewBox(128)
+    setCurrentTrackIndex(0);
+  }
+
+  const handleMrsJxn = () => {
+    setMrmrsViewBox(64)
+    setJxnViewBox(64)
+    setTracks(mrsjxnTracks)
+    setCurrentTrackIndex(0);
+  }
+
+  const handleJxn = () => {
+    setTracks(jxnTracks)
+    setMrmrsViewBox(0)
+    setJxnViewBox(0)
+    setCurrentTrackIndex(0);
+  }
+
+  return (
+    <>
+      <div style={{ minHeight: '90dvh'}}>
+    <nav style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+        <button style={{ all: 'unset' }} onClick={handleMrs}>MRMRS</button>
+        <button style={{ all: 'unset' }} onClick={handleMrsJxn}>MRSJXN</button>
+        <button style={{ all: 'unset' }} onClick={handleJxn}>JXNBLK</button>
+    </nav>
+    <div style={{ position: 'relative', marginTop: '32px', marginBottom: '32px', height: '192px' }}>
+    <div style={{ position: 'absolute', top:0, right: 0, left: 0, bottom: 0, display: 'flex', justifyContent:'center', alignItems: 'center'}}>
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox='0 0 128 128' enableBackground="new 0 0 128 128" xmlSpace="preserve" style={{ width: '192px' }}>
+    <mask id="mrsMask">
+      <rect x="0" y="0" width={mrmrsViewBox} height="128" fill="white" />
+    </mask>
+	<path fill="currentColor" d="M64,22C52,22,8,30,8,58s16,28,32,32l4,16l4-8h32l4,8l4-16c16-4,32-4,32-32S76,22,64,22 M16,66
+		c0-8.8,7.2-16,16-16s16,7.2,16,16s-7.2,16-16,16S16,74.8,16,66 M80,66c0-8.8,7.2-16,16-16s16,7.2,16,16s-7.2,16-16,16
+		S80,74.8,80,66" mask="url(#mrsMask)"/>
+</svg>
+    </div>
+    <div style={{ position: 'absolute', top:0, right: 0, left: 0, bottom: 0, display: 'flex', justifyContent:'center', alignItems: 'center'}}>
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox='0 0 128 128' enableBackground="new 0 0 128 128" xmlSpace="preserve" style={{width: '192px'}}>
+   <mask id="jxnMask">
+      <rect x={jxnViewBox} y="0" width="128" height="128" fill="white" />
+    </mask>
+<g id="Layer_1" mask='url(#jxnMask)'>
+	<path fill="currentColor" d="M64,0v4h12l-2,2l20,8l-2,2l12,4l-4,2l6,6h-4l4,28c2.7,4,4,8.7,4,14s-1.7,10-5,14l-9,24c-4,8-20,20-32,20
+		s-28-12-32-20l-9-24c-3.3-4-5-8.7-5-14s1.3-10,4-14V22l4,2l8-12l2,4L64,0z M72,40c-10.7-6.7-21.3-10-32-10c-5.3,2.7-8.7,8.7-10,18
+		l0,0l-2,26c0,7,9,28,10,30c8,14,24,17,26,17s18-3,26-17c1-2,10-23,10-30l-1-22l-5-12l-2,2l-8-4v4l-10-6 M57,88h14c1.1,0,2,0.9,2,2
+		s-0.9,2-2,2H57c-1.1,0-2-0.9-2-2S55.9,88,57,88 M53,104h22c1.1,0,2,0.9,2,2s-0.9,2-2,2H53c-1.1,0-2-0.9-2-2S51.9,104,53,104"/>
+</g>
+</svg>
+    </div>
+    </div>
+<MusicPlayer tracks={tracks} currentTrackIndex={currentTrackIndex} setCurrentTrackIndex={setCurrentTrackIndex} />
+      </div>
+    <footer style={{ marginTop: '32px'}}>
+        <small style={{ textAlign: 'center', display: 'block' }}>© 2048</small>
+    </footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MusicPlayer.jsx
@@ -0,0 +1,100 @@
+import React, { useState, useRef } from 'react';
+
+const IconPlay = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="currentcolor"><path d="M8 5v14l11-7z"></path></svg>
+  )
+}
+
+const IconPause = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20"><path d="M0 0 H12 V32 H0 z M20 0 H32 V32 H20 z"></path></svg>
+  )
+}
+
+const IconPrev = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" height='16' width='16'><path d="M0 0 H4 V14 L32 0 V32 L4 18 V32 H0 z"></path></svg>
+  )
+}
+
+const IconNext = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" height='16' width='16'>
+      <path d="M0 0 L28 14 V0 H32 V32 H28 V18 L0 32 z"></path>
+    </svg>
+  )
+}
+
+const MusicPlayer = ({ tracks, currentTrackIndex, setCurrentTrackIndex }) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const audioRef = useRef(null);
+
+  const playPause = () => {
+    const status = !isPlaying;
+    setIsPlaying(status);
+    if (status) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  };
+
+  const playTrack = (index) => {
+    setCurrentTrackIndex(index);
+    setIsPlaying(true);
+    setCurrentTime(0); // Reset time for a new track
+  };
+
+  const skipTrack = (direction) => {
+    let index = currentTrackIndex + direction;
+    if (index < 0) index = tracks.length - 1;
+    else if (index >= tracks.length) index = 0;
+    playTrack(index);
+  };
+
+  const onLoadedMetadata = () => {
+    setDuration(audioRef.current.duration);
+  };
+
+  const onTimeUpdate = () => {
+    setCurrentTime(audioRef.current.currentTime);
+  };
+
+  return (
+    <div>
+      <audio src={tracks[currentTrackIndex].url} ref={audioRef} autoPlay={isPlaying}
+             onEnded={() => skipTrack(1)}
+             onLoadedMetadata={onLoadedMetadata}
+             onTimeUpdate={onTimeUpdate} />
+      <div style={{ display: 'flex', gap: '16px', justifyContent: 'center' }}>
+        <button onClick={() => skipTrack(-1)} style={{ border: 0, background: 'transparent' }}><IconPrev /></button>
+        <button onClick={playPause} style={{ borderRadius: '9999px', aspectRatio: 1, display: 'inline-flex', alignItems: 'center', justifyContent: 'center', background: 'transparent', borderWidth: '4px', width: '64px', height: '64px' }}>{isPlaying ? <IconPause /> : <IconPlay />}</button>
+        <button style={{ background: 'transparent', border: 0, }} onClick={() => skipTrack(1)}><IconNext /></button>
+      </div>
+    <p style={{ maxWidth: '640px', margin: '16px auto', textAlign: 'center', fontSize: '32px', fontWeight: 'bold' }}>{tracks[currentTrackIndex].title}</p>
+      <div style={{ width: '100%', backgroundColor: 'transparent', boxShadow: '0 0 0 1px currentColor', margin: '0 auto', maxWidth: '640px' }}>
+        <div style={{ width: `${(currentTime / duration) * 100}%`, height: '8px', backgroundColor: 'black', transition: 'width .1s ease' }}></div>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'space-between', maxWidth: '640px', margin: '4px auto 4px auto' }}>
+        <time style={{ fontSize: '10px' }}>{Math.floor(currentTime / 60)}:{("0" + Math.floor(currentTime % 60)).slice(-2)}</time>
+        <time style={{ fontSize: '10px' }}>{Math.floor(duration / 60)}:{("0" + Math.floor(duration % 60)).slice(-2)}</time>
+      </div>
+      <ul style={{ listStyle: 'none', margin: '16px auto 0 auto', padding: 0, width: '100%', maxWidth: '640px' }}>
+        {tracks.map((track, index) => (
+          <li key={index} style={{ margin: 0, padding: 0, borderBottom: '1px dashed', backgroundColor: currentTrackIndex === index ? 'black' : 'transparent', color: currentTrackIndex === index ? 'white' : 'black' }}>
+            <button onClick={() => playTrack(index)} style={{ all:'unset', borderRadius: 0, outline: 0, textAlign: 'left', width: '100%', padding: '16px', background: 'transparent' }}>
+              {track.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MusicPlayer;
